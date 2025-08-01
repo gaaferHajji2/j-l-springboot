d542bfc1ac9f2056c62038f3c4e0ef01ea19e4d5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-01 19:29:31 +0300

    Adding config to application properties and use them in controller

--- store/src/main/java/com/jafarloka/store/HomeController.java
+++ store/src/main/java/com/jafarloka/store/HomeController.java
@@ -1,13 +1,23 @@
 package com.jafarloka.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${jloka.value}")
+    private String value;
+
     @RequestMapping("/")
-    public String index(){
+    public String index() {
+
+        System.out.println("The app name is: " + appName + ", The value is: " + value);
+
         return "index.html";
     }
 
