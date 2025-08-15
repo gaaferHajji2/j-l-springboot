cbc57ce7d6fca82e228289d09198a104d282749a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-15 20:05:23 +0300

    Adding stripe payment as primary payment way

--- store/src/main/java/com/jafarloka/store/StripePaymentService.java
+++ store/src/main/java/com/jafarloka/store/StripePaymentService.java
@@ -1,8 +1,10 @@
 package com.jafarloka.store;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class StripePaymentService implements PaymentService {
 
     @Override
