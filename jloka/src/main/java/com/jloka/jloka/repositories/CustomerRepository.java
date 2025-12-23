63cfedaf77a752814b877b979b338ec74a02cd9a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-23 15:08:27 +0300

    Remove unused lines

--- jloka/src/main/java/com/jloka/jloka/repositories/CustomerRepository.java
+++ jloka/src/main/java/com/jloka/jloka/repositories/CustomerRepository.java
@@ -1,11 +1,8 @@
 package com.jloka.jloka.repositories;
 
 import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-
 import com.jloka.jloka.models.Customer;
-
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     List<Customer> findByLastName(String lastName);
     Customer findById(long id);
