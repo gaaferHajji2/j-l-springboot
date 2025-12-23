1fbb61997432c5f0610ae1d835c7391b6b805c37
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-23 13:32:25 +0300

    Implement CustomerRepository

---
+++ jloka/src/main/java/com/jloka/jloka/repositories/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.jloka.jloka.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jloka.jloka.models.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+    Customer findById(long id);
+}
