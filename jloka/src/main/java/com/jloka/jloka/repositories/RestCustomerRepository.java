5d9f2c3baf3e3bfb212650187b65b8cc1862923d
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-26 18:12:31 +0300

    Adding Rest Repository for customer

---
+++ jloka/src/main/java/com/jloka/jloka/repositories/RestCustomerRepository.java
@@ -0,0 +1,15 @@
+package com.jloka.jloka.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.jloka.jloka.models.Customer;
+
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
+public interface RestCustomerRepository extends PagingAndSortingRepository<Customer, Long>, 
+    CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String name);
+}
