6a378be6f2fd7758940c203ba60033481f677a1a
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-26 17:40:19 +0300

    Adding Customer Controller

---
+++ jloka/src/main/java/com/jloka/jloka/controllers/CustomerController.java
@@ -0,0 +1,33 @@
+package com.jloka.jloka.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jloka.jloka.models.Customer;
+import com.jloka.jloka.repositories.CustomerRepository;
+
+@Controller
+@RequestMapping(path="/demo")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+    
+    @PostMapping("/add")
+    public @ResponseBody Customer addNewCustomer(@RequestParam String firstName, @RequestParam String lastName) {
+        Customer customer = new Customer(firstName, lastName);
+        customerRepository.save(customer);
+        return customer;
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody Iterable<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+}
