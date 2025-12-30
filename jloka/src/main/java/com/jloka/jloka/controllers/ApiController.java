e0a88d1b35ba4a351af9a2e110f01e7bb07c7c4e
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-30 14:20:05 +0300

    Adding api controller and refactor

---
+++ jloka/src/main/java/com/jloka/jloka/controllers/ApiController.java
@@ -0,0 +1,27 @@
+package com.jloka.jloka.controllers;
+
+import com.jloka.jloka.services.DataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    
+    @Autowired
+    private DataService dataService;
+    
+    @PostMapping("/create-sample")
+    public ResponseEntity<String> createSampleData() {
+        dataService.createSampleData();
+        return ResponseEntity.ok("Sample data created successfully");
+    }
+    
+    @GetMapping("/user/{id}")
+    public ResponseEntity<String> getUserInfo(@PathVariable Long id) {
+        // In a real application, you would return a DTO or entity
+        dataService.displayUserData(id);
+        return ResponseEntity.ok("User data displayed in console");
+    }
+}
--- jloka/src/main/java/com/jloka/jloka/services/DataService.java
+++ jloka/src/main/java/com/jloka/jloka/services/DataService.java
@@ -5,7 +5,6 @@ import com.jloka.jloka.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 
 @Service
