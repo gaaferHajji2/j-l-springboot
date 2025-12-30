f37ed3e65a750563dddf03907b10d76618fd1d10
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-30 14:01:23 +0300

    Comment unused code

--- jloka/src/main/java/com/jloka/jloka/JlokaApplication.java
+++ jloka/src/main/java/com/jloka/jloka/JlokaApplication.java
@@ -2,17 +2,17 @@ package com.jloka.jloka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 
-import com.jloka.jloka.models.Customer;
-import com.jloka.jloka.repositories.CustomerRepository;
+// import com.jloka.jloka.models.Customer;
+// import com.jloka.jloka.repositories.CustomerRepository;
 
 @SpringBootApplication
 public class JlokaApplication {
-	private static final Logger logger = LoggerFactory.getLogger(JlokaApplication.class);
+	// private static final Logger logger = LoggerFactory.getLogger(JlokaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JlokaApplication.class, args);
