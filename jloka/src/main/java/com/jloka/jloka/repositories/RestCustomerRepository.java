004199a05311ebb659952c713739d4e1f4640c6a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-19 14:25:47 +0300

    Remove unused spaces from rest customer repository

--- jloka/src/main/java/com/jloka/jloka/repositories/RestCustomerRepository.java
+++ jloka/src/main/java/com/jloka/jloka/repositories/RestCustomerRepository.java
@@ -1,11 +1,9 @@
 package com.jloka.jloka.repositories;
 
 import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import com.jloka.jloka.models.Customer;
 
 @RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
