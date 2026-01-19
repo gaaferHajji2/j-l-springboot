ac9351adf7236451cdd5163c5fb9d86edb7d8f50
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-19 14:41:52 +0300

    Remove unused spaces

--- jloka/src/main/java/com/jloka/jloka/controllers/ApiController.java
+++ jloka/src/main/java/com/jloka/jloka/controllers/ApiController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    
     @Autowired
     private DataService dataService;
     
