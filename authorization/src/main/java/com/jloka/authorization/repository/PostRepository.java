8433c85cac0281bdf2199dec21fdb6f214c5a7ea
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:41:09 +0300

    Create post repository

--- authorization/src/main/java/com/jloka/authorization/repository/PostRepository.java
+++ authorization/src/main/java/com/jloka/authorization/repository/PostRepository.java
@@ -1,5 +1,18 @@
 package com.jloka.authorization.repository;
 
-public class PostRepository {
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.jloka.authorization.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAll(Pageable pageable);
     
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.user WHERE p.id = :id")
+    Optional<Post> findByIdWithUser(@Param("id") Long id);
 }
