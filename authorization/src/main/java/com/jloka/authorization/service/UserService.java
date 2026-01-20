6eb4b6c359e0a928ae36c5c9587042ff9318e3c4
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:54:23 +0300

    Finish from user service

--- authorization/src/main/java/com/jloka/authorization/service/UserService.java
+++ authorization/src/main/java/com/jloka/authorization/service/UserService.java
@@ -1,5 +1,37 @@
 package com.jloka.authorization.service;
 
-public class UserService {
+import com.jloka.authorization.model.User;
+import com.jloka.authorization.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
     
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+    
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 }
