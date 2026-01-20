b883e28cc2cd00a20e982623e2015ac77a542121
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:25:10 +0300

    Create post model

--- authorization/src/main/java/com/jloka/authorization/model/Post.java
+++ authorization/src/main/java/com/jloka/authorization/model/Post.java
@@ -1,5 +1,26 @@
 package com.jloka.authorization.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+    
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
