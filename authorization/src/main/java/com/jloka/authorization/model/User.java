1cdd9ea561032265d29de88c389181dfd5687727
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:23:10 +0300

    Create user model

--- authorization/src/main/java/com/jloka/authorization/model/User.java
+++ authorization/src/main/java/com/jloka/authorization/model/User.java
@@ -1,5 +1,29 @@
 package com.jloka.authorization.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(unique = true, nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false)
+    private String email;
+    
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Post> posts;
 }
