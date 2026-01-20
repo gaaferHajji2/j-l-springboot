5582f607f4186d4cce77cf08847616617d68bc99
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:35:13 +0300

    Create Post dto with lombok data

--- authorization/src/main/java/com/jloka/authorization/dto/PostDTO.java
+++ authorization/src/main/java/com/jloka/authorization/dto/PostDTO.java
@@ -1,5 +1,24 @@
 package com.jloka.authorization.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class PostDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private Long userId;
+    private String username;
     
+    public PostDTO(Long id, String title, String content, 
+                   LocalDateTime createdAt, Long userId, String username) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.userId = userId;
+        this.username = username;
+    }
 }
