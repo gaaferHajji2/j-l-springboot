de45333023d25253fe5fcec9287d31b571c7eb59
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:37:59 +0300

    Remove unused spaces from post dto

--- authorization/src/main/java/com/jloka/authorization/dto/PostDTO.java
+++ authorization/src/main/java/com/jloka/authorization/dto/PostDTO.java
@@ -11,14 +11,4 @@ public class PostDTO {
     private LocalDateTime createdAt;
     private Long userId;
     private String username;
-    
-    public PostDTO(Long id, String title, String content, 
-                   LocalDateTime createdAt, Long userId, String username) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.userId = userId;
-        this.username = username;
-    }
 }
