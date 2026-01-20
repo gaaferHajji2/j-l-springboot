c0cfd9be62a9c49e6a99cabd190bf8d73705adae
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 14:09:38 +0300

    Create security config

--- authorization/src/main/java/com/jloka/authorization/config/securityConfig.java
+++ authorization/src/main/java/com/jloka/authorization/config/securityConfig.java
@@ -1,5 +1,60 @@
 package com.jloka.authorization.config;
 
-public class securityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jloka.authorization.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
     
+    @Autowired
+    private UserService userService;
+    
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authManagerBuilder =  http.getSharedObject(AuthenticationManagerBuilder.class);
+        authManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
+        return authManagerBuilder.build();  
+
+    //     return http.getSharedObject(AuthenticationManagerBuilder.class)
+    //             .userDetailsService(userService)
+    //             .passwordEncoder(passwordEncoder())
+    //             .and()
+    //             .build();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+            );
+        
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+    }
 }
