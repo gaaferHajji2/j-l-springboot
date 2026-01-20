a6a203ea4d2c0d3188b4c899654aa96a928d9017
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-20 13:34:23 +0300

    Create Auth Response with lombok data

--- authorization/src/main/java/com/jloka/authorization/dto/AuthResponse.java
+++ authorization/src/main/java/com/jloka/authorization/dto/AuthResponse.java
@@ -1,5 +1,10 @@
 package com.jloka.authorization.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
-    
+    private String token;
 }
