7a3d552ebe6ae96932a8be8b5d2a2dbc7f964c37
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-21 17:56:50 +0300

    Create post controller

--- authorization/src/main/java/com/jloka/authorization/controller/PostController.java
+++ authorization/src/main/java/com/jloka/authorization/controller/PostController.java
@@ -1,5 +1,60 @@
 package com.jloka.authorization.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jloka.authorization.dto.PostDTO;
+import com.jloka.authorization.model.Post;
+import com.jloka.authorization.service.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
     
+    @Autowired
+    private PostService postService;
+    
+    @GetMapping
+    public ResponseEntity<Page<PostDTO>> getAllPosts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "desc") String direction) {
+        
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("asc") ? 
+                Sort.Direction.ASC : Sort.Direction.DESC;
+        
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
+        
+        Page<PostDTO> posts = postService.getAllPosts(pageable);
+        return ResponseEntity.ok(posts);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable Long id) {
+        Optional<PostDTO> post = postService.getPostById(id);
+        
+        return post.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping
+    public ResponseEntity<Post> createPost(@NonNull @RequestBody Post post) {
+        Post savedPost = postService.createPost(post);
+        return ResponseEntity.ok(savedPost);
+    }
 }
