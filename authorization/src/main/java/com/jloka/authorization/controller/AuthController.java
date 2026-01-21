4a0e9bb11dc131a9d8839785b5a6155d64e1f305
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-21 17:52:42 +0300

    Create auth controller

--- authorization/src/main/java/com/jloka/authorization/controller/AuthController.java
+++ authorization/src/main/java/com/jloka/authorization/controller/AuthController.java
@@ -1,5 +1,48 @@
 package com.jloka.authorization.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import com.jloka.authorization.dto.AuthRequest;
+import com.jloka.authorization.dto.AuthResponse;
+import com.jloka.authorization.model.User;
+import com.jloka.authorization.service.JwtService;
+import com.jloka.authorization.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
     
+    @Autowired
+    private JwtService jwtService;
+    
+    @Autowired
+    private UserService userService;
+    
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(),
+                authRequest.getPassword()
+            )
+        );
+        
+        String token = jwtService.generateToken(authRequest.getUsername());
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+    
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
 }
