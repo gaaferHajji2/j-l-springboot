3a3fd927b0c6aab48f4303d4da1c66d85553f14c
JafarLoka <gaafer.hajji1995@gmail.com>
2026-02-02 12:18:45 +0300

    Remove unused white spaces

--- validation/src/main/java/com/jloka/validation/handlers/ValidationExceptionHandler.java
+++ validation/src/main/java/com/jloka/validation/handlers/ValidationExceptionHandler.java
@@ -15,9 +15,12 @@ public class ValidationExceptionHandler {
     public ResponseEntity<Map<String, String>> 
         handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-
+        ex.getBindingResult().getFieldErrors().forEach(
+            error -> errors.put(
+                error.getField(), 
+                error.getDefaultMessage()
+            )
+        );
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
-
 }
