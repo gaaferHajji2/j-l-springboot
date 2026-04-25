d7d8f20da9c06922d5688ddff828f0a67937a42b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-25 09:05:14 +0300

    Create the base entity for the project

---
+++ E-commerce/e_commerce/src/main/java/com/jafarloka/e_commerce/e_commerce/domain/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.jafarloka.e_commerce.e_commerce.domain.model;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.MappedSuperclass;;
+
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    private String id;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
