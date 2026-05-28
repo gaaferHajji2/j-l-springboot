be6f79c24028fafafbd1ad1ec6b31dc9af5cee18
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-28 16:04:19 +0300

    Remove unused code & imports

--- store/src/main/java/com/jafarloka/store/StoreApplication.java
+++ store/src/main/java/com/jafarloka/store/StoreApplication.java
@@ -1,8 +1,5 @@
 package com.jafarloka.store;
 
-import com.jafarloka.store.models.Address;
-import com.jafarloka.store.models.Tag;
-import com.jafarloka.store.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,17 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
-
-		var user = new User();
-		user.setName("Jafar Loka");
-		user.setPassword("Test@123");
-
-		Tag tag1 = new Tag("JLoka-01");
-		Tag tag2 = new Tag("JLoka-02");
-
-		user.addTag(tag1);
-		user.addTag(tag2);
-		
-		System.out.println("The user is: " + user);
 	}
 }
