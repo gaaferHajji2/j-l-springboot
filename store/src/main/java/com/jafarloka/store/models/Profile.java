aa977bb545ed17a5e28897623bb6c624d76db0f5
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-28 16:03:47 +0300

    Adding the profile required method and annotation

--- store/src/main/java/com/jafarloka/store/models/Profile.java
+++ store/src/main/java/com/jafarloka/store/models/Profile.java
@@ -2,15 +2,20 @@ package com.jafarloka.store.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "profiles")
 @Getter
 @Setter
+@Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Profile {
@@ -26,4 +31,10 @@ public class Profile {
     private LocalDate dateOfBirth;
     @Column(nullable = false, name = "loyalty_points")
     private int loyaltyPoints;
+    // The @MapsId tell the framework to use the foreign and primary as the join column key.
+    @OneToOne
+    @JoinColumn(name = "id")
+    @MapsId
+    @ToString.Exclude
+    private User user;
 }
