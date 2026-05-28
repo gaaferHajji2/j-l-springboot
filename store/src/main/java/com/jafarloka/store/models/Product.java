1102dd7db36c2d66e5d679d9d0c2c78cc1c4fe0e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-28 18:51:07 +0300

    Create the product entity

---
+++ store/src/main/java/com/jafarloka/store/models/Product.java
@@ -0,0 +1,33 @@
+package com.jafarloka.store.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    // Many Products belong to one Category
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+}
