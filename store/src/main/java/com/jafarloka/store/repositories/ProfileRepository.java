e59ad630b101a7b8f23f3fb45cb24d07e6c31924
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-06-12 09:53:34 +0300

    Adding the profile repository

---
+++ store/src/main/java/com/jafarloka/store/repositories/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.jafarloka.store.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jafarloka.store.models.Profile;
+
+public interface ProfileRepository extends CrudRepository<Profile, Long> {
+    
+}
